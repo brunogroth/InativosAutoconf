f24b6b17ec3f32adaa8e613fa2f0aa106aa8d9f7
brunogroth <brunom.groth@gmail.com>
2023-04-07 23:59:16 -0300

    Feat: Pagination!

--- Frontend/resources/views/list.blade.php
+++ Frontend/resources/views/list.blade.php
@@ -14,13 +14,13 @@
             color: rgba(0, 0, 0, 0.65);
         }
     </style>
-    <div class="site-filter"> 
-    <form class="row gx-3 gy-2 align-items-center" method="get" action="{{route('filter')}}">
+    <div class="site-filter">
+        <form class="row gx-3 gy-2 align-items-center" method="get" action="{{ route('filter') }}">
             @csrf
             <small class="ms-2">Filtro Status</small>
             <div class="col-sm-3">
                 <select class="form-select" aria-label="Default select example" name="status">
-                    <option value= '' selected>Todos</option>
+                    <option value='' selected>Todos</option>
                     <option value="0">Aguardando Pausa</option>
                     <option value="1">Pausado - Aguardando pagamento</option>
                     <option value="2">Aguardando Desativamento</option>
@@ -32,71 +32,74 @@
                 <button class="btn btn-primary" type="submit"> <i class="fa fa-search" aria-hidden="true"></i></button>
             </div>
             <div class="col-auto">
-                <a href="{{route('list')}}" class="btn btn-danger" type="submit"> <i class="fa fa-trash" aria-hidden="true"></i></a>
+                <a href="{{ route('list') }}" class="btn btn-danger" type="submit"> <i class="fa fa-trash"
+                        aria-hidden="true"></i></a>
             </div>
-        </form>    
+        </form>
     </div>
-        <table class="table table-bordered table-wrap shadow-lg">
-            <tr class="thead-primary">
-                <th scope="col">Nome da loja - #ID</th>
-                <th scope="col">URL do site</th>
-                <th scope="col">Criado em</th>
-                <th scope="col">Desativar em</th>
-                <th scope="col">Status</th>
-                <th scope="col">Tempo restante</th>
-                <th scope="col">Editar</th>
-                <th scope="col">Cliente Recuperado?</th>
-                <th scope="col">Deletar</th>
-            </tr>
-            @foreach ($sites->data as $site)
-                <tr
-                    class=" @if ($site->status == 'Recuperado') table-success @elseif($site->time_left <= 2 && $site->time_left > 0) table-warning @endif ">
-                    <th scope="row">{{ $site->name }}</td>
-                    <td>{{ $site->url }}</td>
-                    <td>{{ $site->created_at }}</td>
-                    {{-- condição com bug --}}
-                    <td class="{{-- @if ($site->final_date > $today) expired @else valid @endif --}}">
-                        {{ $site->final_date }}
-                    </td>
-                    <td>{{ $site->status }}</td>
-                    <td {{-- class="@if ($site->time_left > 0) text-warning @else text-danger @endif" --}}>{{ $site->time_left > 0 ? $site->time_left . ' dias' : '-' }}</td>
-                    <td>
-                        <a href="{{ route('edit', $site->id) }}" class="btn btn-warning">Editar</a>
-                    </td>
-                    <td style="text-align: center">
-                        @if ($site->status !== 'Recuperado')
-                            <form method="POST" action="recover">
-                                @csrf
-                                @method('patch')
-                                <input type="hidden" name="id" value="{{ $site->id }}">
-                                <button class="btn btn-success">Recuperar</button>
-                            </form>
-                        @else
-                            <span style="font-size: 2rem">✅</span>
-                        @endif
-                    </td>
-                    <td>
-                        <form method="POST" action="inactivate">
+    <table class="table table-bordered table-wrap shadow-lg">
+        <tr class="thead-primary">
+            <th scope="col">Nome da loja - #ID</th>
+            <th scope="col">URL do site</th>
+            <th scope="col">Criado em</th>
+            <th scope="col">Desativar em</th>
+            <th scope="col">Status</th>
+            <th scope="col">Tempo restante</th>
+            <th scope="col">Editar</th>
+            <th scope="col">Cliente Recuperado?</th>
+            <th scope="col">Deletar</th>
+        </tr>
+        @foreach ($sites->data as $site)
+            <tr
+                class=" @if ($site->status == 'Recuperado') table-success @elseif($site->time_left <= 2 && $site->time_left > 0) table-warning @endif ">
+                <th scope="row">{{ $site->name }}</td>
+                <td>{{ $site->url }}</td>
+                <td>{{ $site->created_at }}</td>
+                {{-- condição com bug --}}
+                <td class="{{-- @if ($site->final_date > $today) expired @else valid @endif --}}">
+                    {{ $site->final_date }}
+                </td>
+                <td>{{ $site->status }}</td>
+                <td {{-- class="@if ($site->time_left > 0) text-warning @else text-danger @endif" --}}>{{ $site->time_left > 0 ? $site->time_left . ' dias' : '-' }}</td>
+                <td>
+                    <a href="{{ route('edit', $site->id) }}" class="btn btn-warning">Editar</a>
+                </td>
+                <td style="text-align: center">
+                    @if ($site->status !== 'Recuperado')
+                        <form method="POST" action="recover">
                             @csrf
                             @method('patch')
                             <input type="hidden" name="id" value="{{ $site->id }}">
-                            <button class="btn btn-danger"><i class="fa fa-trash" aria-hidden="true"></i></button>
+                            <button class="btn btn-success">Recuperar</button>
                         </form>
-                    </td>
-                </tr>
-            @endforeach
-        </table>
-        <div class="container text-center">
-            <div class="row justify-content-end">
-                <div class="col-4">
-                    <nav aria-label="Page navigation example">
-                        <ul class="pagination">
+                    @else
+                        <span style="font-size: 2rem">✅</span>
+                    @endif
+                </td>
+                <td>
+                    <form method="POST" action="inactivate">
+                        @csrf
+                        @method('patch')
+                        <input type="hidden" name="id" value="{{ $site->id }}">
+                        <button class="btn btn-danger"><i class="fa fa-trash" aria-hidden="true"></i></button>
+                    </form>
+                </td>
+            </tr>
+        @endforeach
+    </table>
+    <div class="container text-center">
+        <div class="row justify-content-end">
+            <div class="col-4">
+                <nav aria-label="Page navigation example">
+                    <ul class="pagination">
+                        @foreach ($sites->links as $pagination)
                             <li class="page-item">
-                                <a class="page-link" href='{{ route('list', 'page=1') }}' aria-label="Previous">
-                                    <span aria-hidden="true">&laquo;</span>
+                                <a href="{{ $pagination->url }}" class="{{ $pagination->active ? 'active' : '' }}">
+                                    {{str_contains($pagination->label,'&laquo;') ? 'Anterior' : $pagination->label }}
                                 </a>
                             </li>
-                            {{-- @for ($i = 1; $i <= $sites->last_page; $i++)
+                        @endforeach
+                        {{-- @for ($i = 1; $i <= $sites->last_page; $i++)
                                 <li class="page-item"><a class="page-link"
                                         href='{{ route('list', 'page=' . $i) }}'>{{ $i }}</a></li>
                             @endfor
@@ -105,17 +108,17 @@
                                     <span aria-hidden="true">&raquo;</span>
                                 </a>
                             </li> --}}
-                        </ul>
-                    </nav>
+                    </ul>
+                </nav>
+            </div>
+            <div class="col-4">
+                <div class=" my-2 my-lg-0 d-flex flex-row-reverse mr-5">
+                    <a href='{{ Route('create') }}' class="btn btn-primary my-2 my-sm-0">Criar novo</a>
                 </div>
-                <div class="col-4">
-                    <div class=" my-2 my-lg-0 d-flex flex-row-reverse mr-5">
-                        <a href='{{ Route('create') }}' class="btn btn-primary my-2 my-sm-0">Criar novo</a>
-                    </div>
-                </div>
-                {{-- <form class="form-inline my-2 my-lg-0 -right d-flex flex-row-reverse mr-5">
+            </div>
+            {{-- <form class="form-inline my-2 my-lg-0 -right d-flex flex-row-reverse mr-5">
             <button class="btn btn-primary my-2 my-sm-0" type="submit">Criar novo</button>
         </form> --}}
 
-            </div>
-        @endsection
+        </div>
+    @endsection
